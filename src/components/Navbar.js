0ff6f0366819dcf5a49a0b66662b8b3c58edb20f
Albin-Binu <albinbinuk17@gmail.com>
2025-06-18 21:41:10 +0530

    edited

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,14 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 
-import linkdin from '../assets/img/instagram.svg'
-import facebook from '../assets/img/facebook.svg';
-import instagram from '../assets/img/instagram.svg';
-import tgicon from '../assets/img/telegram.svg'
-import { HashLink } from 'react-router-hash-link';
-import {
-  BrowserRouter as Router
-} from "react-router-dom";
-
+import linkdin from "../assets/img/instagram.svg";
+import facebook from "../assets/img/facebook.svg";
+import instagram from "../assets/img/instagram.svg";
+import tgicon from "../assets/img/telegram.svg";
+import { BrowserRouter as Router } from "react-router-dom";
 
 export const NavBar = () => {
-
-  const [activeLink, setActiveLink] = useState('home');
+  const [activeLink, setActiveLink] = useState("home");
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
@@ -23,53 +18,94 @@ export const NavBar = () => {
       } else {
         setScrolled(false);
       }
-    }
+    };
 
     window.addEventListener("scroll", onScroll);
 
     return () => window.removeEventListener("scroll", onScroll);
-  }, [])
+  }, []);
 
   const onUpdateActiveLink = (value) => {
     setActiveLink(value);
-  }
+  };
 
   return (
     <Router>
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="/">
-            <h1 className="navbar-name">Albin<span className="navbar-lastname">Binu</span></h1>
+            <h1 className="navbar-name">
+              Albin<span className="navbar-lastname">Binu</span>
+            </h1>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-              <Nav.Link href="#connect" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Contact</Nav.Link>
+              <Nav.Link
+                href="#home"
+                className={
+                  activeLink === "home" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("home")}
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                href="#skills"
+                className={
+                  activeLink === "skills" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("skills")}
+              >
+                Skills
+              </Nav.Link>
+              <Nav.Link
+                href="#connect"
+                className={
+                  activeLink === "projects"
+                    ? "active navbar-link"
+                    : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("projects")}
+              >
+                Contact
+              </Nav.Link>
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
                 {/* linkdin icon */}
-                <a href="https://linkdin.com/"><img src={linkdin} alt="" /></a>
+                <a href="https://linkdin.com/">
+                  <img src={linkdin} alt="" />
+                </a>
                 {/* fb icon */}
-                <a href="https://facebook.com/albin.binu.526/"><img src={facebook} alt="" /></a>
+                <a href="https://facebook.com/albin.binu.526/">
+                  <img src={facebook} alt="" />
+                </a>
                 {/* instagram icon */}
-                <a href="https://instagram.com/albinbinu.me"><img src={instagram} alt="" /></a>
+                <a href="https://instagram.com/albinbinu.me">
+                  <img src={instagram} alt="" />
+                </a>
                 {/* tg icon */}
-                <a href="t.me/Albin_binu"><img src={tgicon} alt="" /></a>
+                <a href="t.me/Albin_binu">
+                  <img src={tgicon} alt="" />
+                </a>
               </div>
-              <HashLink to='#connect'>
-                  <a href="https://albincv.netlify.app/">
-                <button className="vvd"><span>Download<span className="cnt"> CV</span> </span></button>
-                  </a>
-              </HashLink>
+              <a
+                href="https://albincv.netlify.app/"
+                rel="noopener noreferrer"
+              >
+                <button className="vvd">
+                  <span>
+                    Download <span className="cnt">CV</span>
+                  </span>
+                </button>
+              </a>
             </span>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </Router>
-  )
-}
+  );
+};
